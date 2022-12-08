a10b0a61eb1b608178fb79fa51c243b8bbdce471
Leandro Noskoski <leandro@alternativalinux.net>
2022-12-08 09:00:54 -0300

    docker adjusts

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,2 @@
-use saslquota
 
-CREATE TABLE `Log` ( `sasl_username` VARCHAR(320) NOT NULL , `date` DATETIME(6) NOT NULL , PRIMARY KEY (`sasl_username`, `date`)) ENGINE = InnoDB;
+CREATE TABLE `saslquota_log` ( `sasl_username` VARCHAR(320) NOT NULL , `date` DATETIME(6) NOT NULL , PRIMARY KEY (`sasl_username`, `date`)) ENGINE = InnoDB;
