88dec12e37bd23e53c63e8b20769e436ba55a459
Leandro Noskoski <leandro@alternativalinux.net>
2023-02-01 17:51:10 -0300

    ssl

--- saslquota.py
+++ saslquota.py
@@ -8,13 +8,14 @@ __site__	= "www.alternativalinux.net"
 __projectpage__ = "https://github.com/noskoski/postfix_saslauth"
 __copyright__   = "Copyright 2020, Alternativa Linux"
 __license__ 	= "GPL"
-__version__ 	= "1.5"
+__version__ 	= "1.9"
 __maintainer__ 	= "Leandro A. Noskoski"
 __email__ 	= "leandro@alternativalinux.net"
 __status__ 	= "Production"
 
-import os,socket,struct,sys,time, logging, re,  mysql.connector, syslog, errno, signal, threading, unicodedata,json
+import os,socket,struct,sys,time, logging, re,  mysql.connector, syslog, errno, signal, threading, unicodedata,json,ssl
 from logging.handlers import SysLogHandler
+from mysql.connector.constants import ClientFlag
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -144,7 +145,7 @@ class Job(threading.Thread):
                         _msg = "Sorry you send too much mails, wait and send it later..."
 
             try:
-                _con = mysql.connector.connect(host=_conf["_myhost"], user=_conf["_myuser"], passwd=_conf["_mypasswd"], db=_conf["_mydb"])
+                _con = mysql.connector.connect(host=_conf["_myhost"], user=_conf["_myuser"], passwd=_conf["_mypasswd"], db=_conf["_mydb"], ssl_cert=_conf["_ssl_cert"], ssl_key=_conf["_ssl_key"], ssl_ca=_conf["_ssl_ca"])
                 _cursor = _con.cursor()
                 _cursor.execute("select count(*) from saslquota_log where sasl_username =%s  and `date` >= DATE_SUB(now(6), INTERVAL %s SECOND)",(self.__sasl_username,_period,))
                 record = _cursor.fetchone()
@@ -209,7 +210,8 @@ def service_shutdown(signum, frame):
 
 def Main():
     #try to connect at the start
-    _con = mysql.connector.connect(host=_conf["_myhost"], user=_conf["_myuser"], passwd=_conf["_mypasswd"], db=_conf["_mydb"])
+
+    _con = mysql.connector.connect(host=_conf["_myhost"], user=_conf["_myuser"], passwd=_conf["_mypasswd"], db=_conf["_mydb"], ssl_cert=_conf["_ssl_cert"], ssl_key=_conf["_ssl_key"], ssl_ca=_conf["_ssl_ca"], client_flags=[ClientFlag.SSL])
     _con.close()
 
     socket.setdefaulttimeout(int(_conf["_bindtimeout"]))
